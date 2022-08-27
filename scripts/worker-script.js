911ed6356087f43de37f1c6e9f14217be03a00ac
nimo <immayounggod@gmail.com>
2022-08-27 22:29:14 +0400

    updated worker info

--- scripts/worker-script.js
+++ scripts/worker-script.js
@@ -1,12 +1,33 @@
-let response = fetch("https://pcfy.redberryinternship.ge/api/teams");
+// let response = fetch("https://pcfy.redberryinternship.ge/api/teams");
 
-fetch("https://pcfy.redberryinternship.ge/api/teams")
-    .then(response => {
-       console.log( response.json());
-    //    console.log(response.data);
-    })
-    .catch(error => {
-       console.log("err"+ error);
+// fetch("https://pcfy.redberryinternship.ge/api/teams")
+//     .then(response => {
+//         let x = response.json();
+//        console.log(x);
+//     //    console.log(response.data);
+//     })
+//     .catch(error => {
+//        console.log("err"+ error);
+//     });
+
+
+
+async function getTeams()
+{
+fetch("https://pcfy.redberryinternship.ge/api/teams") //1
+  .then((response) => response.json()) //2
+  .then((user) => {
+    let ret = user.data;
+    console.log(ret);
+    ret.forEach(element => {
+        let a = document.createElement("a");
+            a.id= element.id;
+            a.innerText = element.name;
+            a.className="dropdown-item";
+            document.getElementsByClassName("dropdown-menu")[0].appendChild(document.createElement("li").appendChild(a)); 
     });
+    return(user.data); //3
+  });
+}
 
-
+getTeams();
