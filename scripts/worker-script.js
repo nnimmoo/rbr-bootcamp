4502c3678828b409d51bdbec74db9f7fe3a33942
nimo <immayounggod@gmail.com>
2022-08-28 23:53:48 +0400

    added validation

--- scripts/worker-script.js
+++ scripts/worker-script.js
@@ -27,7 +27,6 @@ async function getTeams() {
         a.id = element.id;
         a.innerText = element.name;
         a.addEventListener('click', function () {
-          let children = document.querySelector(".dropdown-content").children;
           document.getElementsByClassName("dropbtn")[0].textContent = element.name;
         });
         document.getElementById("drp-team").appendChild(a);
@@ -49,7 +48,6 @@ async function getPositions() {
         a.id = element.id;
         a.innerText = element.name;
         a.addEventListener('click', function () {
-          // let children = document.querySelector(".dropdown-content").children;
           document.getElementsByClassName("dropbtn")[1].textContent = element.name;
         });
         document.getElementById("drp-position").appendChild(a);
@@ -59,3 +57,39 @@ async function getPositions() {
 }
 
 getPositions();
+
+
+function validation(){
+  let temp= document.getElementById("username").value;
+  let valid= true;
+  console.log('aq var dzia');
+  if (!(temp.length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp))){
+    valid = false;
+    console.log("wrong name");
+    return;
+  }
+  console.log("stage 0");
+  temp = document.getElementById("lastname").value;
+  if(!(temp.length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp))){
+      valid = false;
+      console.log("wrong surname");
+      return;
+    }
+    console.log("stage 1");
+  
+  if(document.getElementsByClassName("dropbtn")[0].textContent == "თიმი"){
+    console.log("wrong თიმი");
+    return;
+  } 
+  if(document.getElementsByClassName("dropbtn")[1].textContent == "პოზიცია"){
+    console.log("wrong პოზიცია");
+    return;
+  }
+  temp= document.getElementById("email").value;
+  if( /^[a-zA-Z]+$/.test(temp) && !temp.endsWith("@redberry.ge")){
+    console.log("wrong email");
+    return;
+  }
+  console.log("everything good")
+  location.assign("../pages/laptop-info.html")
+}
