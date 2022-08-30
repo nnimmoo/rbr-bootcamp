2c8950d12c3b115a2da85f03db0adda4052bbe27
nimo <immayounggod@gmail.com>
2022-08-31 00:00:26 +0400

    updated worker styling

--- scripts/worker-script.js
+++ scripts/worker-script.js
@@ -32,7 +32,7 @@ async function getTeams() {
         document.getElementById("drp-team").appendChild(a);
       });
     });
-    console.log("done");
+  console.log("done");
 }
 
 getTeams();
@@ -53,41 +53,46 @@ async function getPositions() {
         document.getElementById("drp-position").appendChild(a);
       });
     });
-    console.log("done");
+  console.log("done");
 }
 
 getPositions();
 
 
-function validation(){
-  let temp= document.getElementById("username").value;
-  let valid= true;
+function validation() {
+  let temp = document.getElementById("username");
+  let valid = true;
   console.log('aq var dzia');
-  if (!(temp.length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp))){
-    valid = false;
+  if (!((temp.value).length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp.value))) {
+    document.getElementById("1").classList += "invalid";
     console.log("wrong name");
     return;
   }
   console.log("stage 0");
   temp = document.getElementById("lastname").value;
-  if(!(temp.length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp))){
-      valid = false;
-      console.log("wrong surname");
-      return;
-    }
-    console.log("stage 1");
-  
-  if(document.getElementsByClassName("dropbtn")[0].textContent == "თიმი"){
+  if (!(temp.length > 2 && (/^[\u10d0-\u10f0]*$/).test(temp))) {
+    valid = false;
+    console.log("wrong surname");
+    return;
+  }
+  console.log("stage 1");
+
+  if (document.getElementsByClassName("dropbtn")[0].textContent == "თიმი") {
     console.log("wrong თიმი");
     return;
-  } 
-  if(document.getElementsByClassName("dropbtn")[1].textContent == "პოზიცია"){
+  }
+  if (document.getElementsByClassName("dropbtn")[1].textContent == "პოზიცია") {
     console.log("wrong პოზიცია");
     return;
   }
-  temp= document.getElementById("email").value;
-  if( /^[a-zA-Z]+$/.test(temp) && !temp.endsWith("@redberry.ge")){
+  temp = document.getElementById("email").value;
+  if (/^[a-zA-Z]+$/.test(temp) && !temp.endsWith("@redberry.ge")) {
     console.log("wrong email");
+    return;
+  }
+  temp = document.getElementById("number").value;
+  if (!(/^(\+?995)?(79\d{7}|5\d{8})$/.test(temp.replace(/\s+/g, '')))) {
+    console.log("wrong phone");
     return;
   }
   console.log("everything good")
